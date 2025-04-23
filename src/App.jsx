19cc65345f085ad817d7013628254d95a2616834
gauravjakhar <jakhargaurav001@gmail.com>
2025-04-23 10:55:56 +0530

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
 import './App.css';
 import CustomRoutes from './routes/CustomRoutes';
+import Background from './components/Background/Background';
 
 function App() {
-  
-   return (
-    <div className="outer-pokedex">
-    <h1 id="pokedex-heading" >
-      <Link to ='/'> Pokedex </Link> </h1>
-      <CustomRoutes />
-    </div>
-  )
+    return (
+        <>
+            <Background />
+            <div className="app-container">
+                <div className="outer-pokedex">
+                    <h1 id="pokedex-heading">
+                        <Link to='/'>Pokedex</Link>
+                    </h1>
+                    <CustomRoutes />
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Background.css';
+
+function Background() {
+    return (
+        <div className="background-container">
+            <div className="gradient-1"></div>
+            <div className="gradient-2"></div>
+            <div className="gradient-3"></div>
+            <div className="gradient-4"></div>
+        </div>
+    );
+}
+
+export default Background; 
